209608a8966dc4d8fe5370b05f734cd33c3fb4a4
lesliedahlberg <leslieulfandre@gmail.com>
2014-09-19 15:41:07 +0200

    Basic Structure

---
+++ structs.c
@@ -0,0 +1,37 @@
+användare
+	användar_id
+	personnummer
+	förnamn
+	efternamn
+	adress
+	användartyp
+	användarnamn
+	lösenord
+	aktiv
+
+konto
+	kontonummer
+	användare
+	pengar
+	aktiv
+
+transaktion
+	konto_från
+	konto_till
+	summa
+	datum
+
+begäran_nytt_konto
+	användar_id
+	aktiv
+
+begäran_ta_bort_konto
+	användar_id
+	aktiv
+
+användare.data:
+användar_id:uppgift:value;
+
+
+
+"användar_id", "personnummer", "förnamn", "efternamn", "adress", "användartyp", "användarnamn", "lösenord", "aktiv"
